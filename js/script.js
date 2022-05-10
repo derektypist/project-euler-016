d9ad3dd76047970abec8ff363f13f4a8f8675036
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-05-10 10:10:06 +0000

    Updated script.js - upper number limit

--- js/script.js
+++ js/script.js
@@ -5,11 +5,11 @@ function getNumberInfo() {
     // Get the Value of the Input Field
     let num = document.getElementById("mynumber").value;
     // Check if the input is valid
-    if (isNaN(num) || num.toString().length == 0 || num<1 || num>2000 || !Number.isInteger(Number(num))) {
-        txt += `Invalid Input.  Please enter a whole number between 1 and 2000.`;
+    if (isNaN(num) || num.toString().length == 0 || num<1 || num>1020 || !Number.isInteger(Number(num))) {
+        txt += `Invalid Input.  Please enter a whole number between 1 and 1020.`;
     } else {
         txt += `You have entered the number ${num}. <p>`;
-        txt += `The sum of the digits of 2<sup>${num}</sup> is ${powerDigitSum(num)}`;
+        txt += `The sum of the digits of 2<sup>${num}</sup> is ${powerDigitSum(num)}.`;
     }
 
     // Display Information in the Browser
